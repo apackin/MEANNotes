e097f9f5d8dbc628e4707fba51037f64a344816d
apackin <apackin@gmail.com>
2016-02-28 14:07:50 -0700

    Error handling in Express

--- Express.js
+++ Express.js
@@ -13,15 +13,40 @@ app.use('/files', express.static(path.join(__dirname, "public", "static")));
 // link to router in api (included below)
 app.use('/api/router', require('./api/router.js'));
 
+// Error handling for nonexistant paths
+app.use(function (req, res, next) {
 
-app.get('/broken', function(req,res){
-	res.sendStatus(500);
+    if (path.extname(req.path).length > 0) {
+        res.status(404).end();
+    } else {
+        next(null);
+    }
+
 });
 
+
+
+// forbidden path
 app.get('/forbidden', function(req,res){
 	res.sendStatus(403);
 });
 
+// send main index file
+app.get('/*', function (req, res) {
+    res.sendFile(app.get('indexHTMLPath'));
+});
+
+// Specific internal server error
+app.get('/broken', function(req,res){
+	res.sendStatus(500);
+});
+
+// Error catching endware for next() after database calls.
+app.use(function (err, req, res, next) {
+    console.error(err, typeof next);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
 
 module.exports = app;
